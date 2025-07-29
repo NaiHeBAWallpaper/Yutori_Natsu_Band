6d0c679c2c25ec1b9c69f06dc7508ba3e3f927a6
NaiHe <2794096231@qq.com>
2025-07-29 16:15:54 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -213,7 +213,12 @@ function playVoiceline() {
 
 	setTimeout(function () {
 		acceptingClick = true;
-		currentVoiceline = clamp(currentVoiceline + 1, 0, AUDIO_DETAIL.length);
+		// 当达到末尾时循环回到第一句
+		if (currentVoiceline >= AUDIO_DETAIL.length) {
+			currentVoiceline = 1;
+		} else {
+			currentVoiceline = currentVoiceline + 1;
+		}
 	}, trackDetails.time);
 
 	for (let i = 0; i < trackDetails.count; i++) {
